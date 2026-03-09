9de4d0639606ea60bb213dd22c0fe9c317d1c3df
enzun123 <enzun123@gmail.com>
2026-03-09 14:54:03 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -282,4 +282,8 @@ for i in test_list:
     
 #for 반복문2
 number = 0
-for score in [90, 25, 67, 45, 93]
+for score in [90, 25, 67, 45, 93]:
+    number +=1
+    
+    if score > 60:
+        print("%d번 학생은 합격입니다 ")
