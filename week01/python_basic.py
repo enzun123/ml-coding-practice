924f8ee027dd1b09dd0d3b91ae9d3670aa1e1b93
enzun123 <enzun123@gmail.com>
2026-03-09 16:33:22 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -422,7 +422,7 @@ f.close()
 #파일 쓰기 
 f = open('example.txt', 'w')
 for i in range(1, 6):
-    data = '%d번째 줄입니다. \n' % i
+    data = '%d번째 줄입니다.\n' % i
     f.write(data)
 f.close()
 
@@ -452,7 +452,7 @@ for line in lines:
 
 f.close()
 
-#파일 모드 'r' read()
+# 파일 모드 'r' read()
 f = open('example.txt', 'r')
 data = f.read()
 f.close()
