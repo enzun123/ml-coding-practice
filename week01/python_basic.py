34022ddaea8eeafdcdfee5c6eb2ee459ec0629b5
enzun123 <enzun123@gmail.com>
2026-03-09 15:29:33 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -381,4 +381,10 @@ print(list(range(5)))
 print(list(range(5, 10)))
 print(list(range(5, 10 ,2)))
 
-for i in range(5)
+for i in range(5, 20 ,3):
+    print(i)
+    
+#입략한 s의 길이를 반환
+len("Python")
+
+pri
