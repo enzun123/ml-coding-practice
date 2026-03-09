87d1db3ef5b29584a5f83cab188e68974803a8c3
enzun123 <enzun123@gmail.com>
2026-03-09 15:41:21 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -384,7 +384,7 @@ print(list(range(5, 10 ,2)))
 for i in range(5, 20 ,3):
     print(i)
     
-#입략한 s의 길이를 반환
-len("Python")
+#입력한 s의 길이를 반환
+len('Python')
 
-print(sorted([3, 0. 2, 1]))
+print(sorted)
