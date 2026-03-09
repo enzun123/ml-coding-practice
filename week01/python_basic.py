0ad4a5cb32ab12faf37d9990023b6a558ae2afe7
enzun123 <enzun123@gmail.com>
2026-03-09 14:55:03 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -288,4 +288,9 @@ for score in [90, 25, 67, 45, 93]:
     if score > 60:
         print("%d번 학생은 합격입니다." % number)
     else:
-        print("%d번 학생")
+        print("%d번 학생은 불합격입니다." % number)
+    
+#while 문
+i = 0
+while i < 5:
+    
