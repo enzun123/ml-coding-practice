eee29a6bf5fed445ee45db1908a2c5197cdc18c3
enzun123 <enzun123@gmail.com>
2026-03-16 15:48:27 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -24,4 +24,5 @@ sample_df = pd.read_csv(file_url,index_col=0)
 
 print(sample_df['var_5'])
 
-#print(sample_df['var_1', 'var_2])   #[ ] 안에는 하나의 값만 들어가
+#print(sample_df['var_1', 'var_2])   #[ ] 안에는 하나의 값만 들어갈 수 있음
+print(sample_df[['var_1', 'var_4']])
