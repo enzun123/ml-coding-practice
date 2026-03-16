1834885f8a1c8d185703ef17c662f38336d26090
enzun123 <enzun123@gmail.com>
2026-03-16 17:00:28 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -58,7 +58,7 @@ print(less2015.head())
 
 print((netflix['release_year'] > 2015) & (netflix['type'] == 'TV show'))
 
-more2015_tv = netflix[(netflix['release_year'] > 2015) | (netflix['type'] == 'TV show')]
+more2015_tv = netflix[(netflix['release_year'] > 2015) or (netflix['type'] == 'TV show')]
 print(more2015_tv.head())
 
 more2015_or_tv = netflix[(netflix['release_year'] > 2015) | (netflix['type'] == 'TV show')]
