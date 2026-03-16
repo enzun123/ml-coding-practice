fcd1ef743094d9a2e44da1dc25edf09d6d949fcc
enzun123 <enzun123@gmail.com>
2026-03-16 17:10:32 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -39,7 +39,7 @@ print(sample_df.iloc[0:2])
 print(sample_df.iloc[0:3])
 print(sample_df.iloc[0:3,2:4])          # 컬럼까지 동시에 인덱싱
 
-print(sample_df.drop(['var_1','var_3'], axis=1))
+print(sample_df.drop(['var_1','var_3'], axis=1))    #컬럼으
 print(sample_df.drop(['var_1','var_2'], axis=1))
 print(sample_df.drop(['a','b','c'], axis=0))
 
