404a64113c106cd68a21851933923ba2cb461fa6
enzun123 <enzun123@gmail.com>
2026-03-16 16:20:56 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -93,4 +93,6 @@ print(sample_df.reset_index(drop=True))             # 기존 인덱스는 제거
 print(sample_df.set_index('var_1'))
 
 
-print(sam)
+print(sample_df.describe())
+print(sample_df.std())
+print(sample_df.agg(['count',]))
