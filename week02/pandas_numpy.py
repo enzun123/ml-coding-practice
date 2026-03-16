5bde3de4c1d3c03d1410b304f1481d0917a8451e
enzun123 <enzun123@gmail.com>
2026-03-16 16:54:58 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -216,8 +216,9 @@ print(np.arange(8).reshape(2, 4) +10)
 a = np.arange(8).reshape(2, 4) ** 2
 print(a)
 
-print(a.sum())      #모든 요소의 합
-print(a.mean())     #모든 요소의 평균
+print(a.sum())      # 모든 요소의 합
+print(a.mean())     # 모든 요소의 평균
 print(a.mean(axis = 0))     # 열을 기준으로 연산
 
-print(a.min())  
+print(a.min())      # 모든 요소 중 최솟값
+print(a.max())      #
