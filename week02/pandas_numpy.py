c9743889f04a348159468b897caa77960c750e80
enzun123 <enzun123@gmail.com>
2026-03-16 16:32:26 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -133,6 +133,8 @@ data = {
     'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
     'math': [88, 92, 85, 95, 90],
     'science': [80, 85, 88, 92, 85],
-    'english': 
-    
+    'english': [90, 87, 85, 88, 92]    
 }
+
+# DataFrame 생성
+df = ㅔ
