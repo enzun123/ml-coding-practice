712d0451bd92aa3613d98e51c30ee6008969f6d2
enzun123 <enzun123@gmail.com>
2026-03-16 16:22:56 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -98,4 +98,6 @@ print(sample_df.std())
 print(sample_df.agg(['count','mean', 'std', 'min', 'max']))
 
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/iris.csv'
-ir
+iris = pd.read_csv(file_url)
+
+print(iris)
