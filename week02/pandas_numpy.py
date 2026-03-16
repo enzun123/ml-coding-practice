ce625c0461bf9cda4d4205d4c00de9f4d612501b
enzun123 <enzun123@gmail.com>
2026-03-16 16:48:58 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -199,5 +199,6 @@ print(np_a @ np_b)
 
 print(np.random.randint(11))
 print(np.random.randint(50, 71))
-print(np.random.randint())
+print(np.random.randint(50, 71, 10))
+print(np.random.randint(['red', 'green',]))
 
