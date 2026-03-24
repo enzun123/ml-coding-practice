f210a272a57a794b107a38adbdb1c005455039d8
enzun123 <enzun123@gmail.com>
2026-03-24 17:46:57 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -307,3 +307,7 @@ plt.bar(x, y2, color='deeppink', alpha=0.7, width=0.5, label='Survived')
 plt.xlabel('Parch')
 plt.ylabel('Survived Count', color='deeppink')
 plt.tick_params(axis='y', labelcolor='deeppink')
+plt.legend(loc='upper right')
+
+# 제목 설정 
+plt.syb
