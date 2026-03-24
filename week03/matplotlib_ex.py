1acbf23572299b6b27d5f9175cbfb602427ed518
enzun123 <enzun123@gmail.com>
2026-03-24 17:22:46 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -253,4 +253,7 @@ plt.errorbar(fare_means.index, fare_means, yerr=fare_std, fmt='o',
 
 plt.title('Error Bar Plot of Fare by Parch')
 plt.xlabel('Parch')
-plt
+plt.ylabel('Fare')
+plt.xticks(fare_means.index)
+plt.legend()
+plt.savefig
