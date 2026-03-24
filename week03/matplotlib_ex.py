cc1399e2fa34673428ceb7c3fa4a69ea73187032
enzun123 <enzun123@gmail.com>
2026-03-24 17:21:46 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -249,4 +249,6 @@ plt.figure(figsize=(10, 6))
 
 # 에러바 셍성
 plt.errorbar(fare_means.index, fare_means, yerr=fare_std, fmt='o',
-             )
+             capsize=5, capthick=1, label='Fare')
+
+plt.title('')
