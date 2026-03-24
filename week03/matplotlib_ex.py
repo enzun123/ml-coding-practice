a6af562b63f288661f0384f0aadcfb9da261185f
enzun123 <enzun123@gmail.com>
2026-03-24 10:32:23 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -81,7 +81,7 @@ plt.close()
 
 """### **산점도 그래프 : 나이와 요금, 생존 여부 확인하기**"""
 
-print(titanic.infro(), '\n')
+rint(titanic.infro(), '\n')
 
 #결측지 처리
 itanic = titanic.dropna(subset=['Age', 'Fare', 'Survived'])
