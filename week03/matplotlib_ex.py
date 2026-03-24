4c0ef9db36eca942abe63ce4d16966671a59d8c6
enzun123 <enzun123@gmail.com>
2026-03-24 10:16:45 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -70,4 +70,6 @@ plt.ylabel('Gender')
 plt.legend(bars, ['Survived - Female', 'Survived - Male'], loc='upper right')
 
 # 차이 강조를 위해 수평선 추가
-plt.axvline(x=survived_counts['Male'], color='Gray',)
+plt.axvline(x=survived_counts['Male'], color='Gray', linestyle='--', Linewidth=1)
+
+#
