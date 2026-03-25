a2b1d16faeb5b20fdc103d31d0ea47704e649dba
enzun123 <enzun123@gmail.com>
2026-03-25 11:29:40 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -290,7 +290,7 @@ y2 = parch_counts[1].values # 생존자
 
 plt.figure(figsize=(10, 10))
 
-# plt.subplot() 함수를 사용하여 하나의 그림은 figure에 개별 서브플롯을 하나씩 추가
+# plt.subplot() 함수를 사용하여 하나의 그림인 figure에 개별 서브플롯을 하나씩 추가
 # 첫 번째 서브플롯 설정(선 그래프)
 plt.subplot(2, 1, 1)
 # 선 그래프 그리기
@@ -335,7 +335,7 @@ parch_counts = titanic.groupby('Parch')['Survived'].value_counts().unstack().fil
 print(parch_counts)
 
 # x, y 값 설정
-x = parch_counts.index.astype(str)  #데이터 프레임의 인덱스를 문자열로 변환
+x = parch_counts.index.astype(str)  # 데이터 프레임의 인덱스를 문자열로 변환
 y1 = parch_counts[0].values # 사망자
 y2 = parch_counts[1].values # 생존자
 
