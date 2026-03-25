95ace45f77ef77106f95cc4073b2d3459bf4c087
enzun123 <enzun123@gmail.com>
2026-03-25 10:14:15 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -108,7 +108,7 @@ print(survived_counts)
 # 파이 차트 그리기
 plt.figure(figsize=(8, 8))
 plt.pie(survived_counts, labels=['Not Survived', 'Survived'],colors=['orange', 'gold'],
-        autopct='%01.f%%', startangle=90, shadow=True, explode=(0, 0.1))
+        autopct='%0.1f%%', startangle=90, shadow=True, explode=(0, 0.1))
 
 plt.title('Survival Distrbuition on the Titanic')
 plt.savefig('Figure05.png')
