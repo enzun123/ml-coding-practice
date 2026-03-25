91a0e1b042f65d5a11163a224283dca92513fad6
enzun123 <enzun123@gmail.com>
2026-03-25 10:52:22 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -19,7 +19,7 @@ print(titanic.info())
 pclass_survived_mean = titanic.groupby('Pclass')['Survived'].mean().reset_index()
 pclass_survived_mean
 
-# 맷플룻립 라이브러리 블러오기
+# 맷플롯립 라이브러리 불러오기
 import matplotlib.pyplot as plt
 
 # 선 그래프 그리기
