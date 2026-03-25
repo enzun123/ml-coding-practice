49c3100c049dd46647fa953e796c458fdcfd1093
enzun123 <enzun123@gmail.com>
2026-03-25 11:09:22 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -239,7 +239,7 @@ plt.close()
 fare_means = titanic.groupby('Parch')['Fare'].mean() # 평균
 print(fare_means, '\n')
 
-fare_std = titanic.groupby('Parch')['Fare'].std()   #표준 편차
+fare_std = titanic.groupby('Parch')['Fare'].std()   # 표준 편차
 print(fare_std)
 
 """* 에러바는 데이터의 표준 편차를 나타내는 경우에는 길이가 길수록 해당 그룹의 데이터가 퍼져 있음을 의미"""
