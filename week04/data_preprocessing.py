d6254457be35e6291518989de489beabc8cc6525
enzun123 <enzun123@gmail.com>
2026-03-30 08:45:46 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 # 사본 라이브러리 불러오기
-import seaborn as sus
+import seaborn as sns
 
 # **팁(tips) 데이터셋 불러오기**
 tips= sns.load_dataset('tips')
 print(tips.head())
+
+tips.info()
 
