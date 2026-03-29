584a8f8cd0f06262b9c55c9d01f339c6c041b569
enzun123 <enzun123@gmail.com>
2026-03-30 08:56:46 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -45,3 +45,6 @@ sns.countplot(x='time', hue='day', data=tips, palette='Set2', ax=ax2)
 
 ax1.set_title('Frequency of Tips by Time')
 ax2.set_title('Frequency of Tips by Time and Day')
+plt.savefig('./week04/Seaborn_Figure02.jpg')
+
+#
