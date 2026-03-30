c092e0449ef16451f34d73f29ef74e47d9d7c066
enzun123 <enzun123@gmail.com>
2026-03-30 11:27:16 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -59,7 +59,7 @@ imputer.fit(housing_num)
 print(imputer.statistics_)           # imputer 결과 값
 print(housing_num.median().values)   # 수동으로 계산한 중간값
 
-# 훈련 세트의 누락값을 imputer가 학습반 값으로 채우기
+# 훈련 세트의 누락값을 imputer가 학습한 값으로 채우기
 X = imputer.transform(housing_num)
 
 imputer.feature_names_in_
