8c31fc42fa525ae9f150ab7fcca5eecd0e58c3a9
enzun123 <enzun123@gmail.com>
2026-03-30 09:49:02 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -73,7 +73,7 @@ plt.savefig('./week04/Seaborn_Figure04.jpg')
 
 # **조인트 그래프**
 # joimplot( ) 그리기
-sns.jointplot(x='size')
+sns.jointplot(x='size', y='tip', data=tips, kin)
 # **관계 그래프**
 # pairplot( ) 그리기
 sns.pairplot(data=tips, hue='sex', diag_kind='hist', palette='husl')
