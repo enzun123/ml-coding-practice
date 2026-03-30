d914619efbb0e3b1479a8f0e80c0e49b2909e055
enzun123 <enzun123@gmail.com>
2026-03-30 10:20:11 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -23,4 +23,5 @@ housing = load_housing_data()
 from sklearn.model_selection import train_test_split
 
 housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
                                )
