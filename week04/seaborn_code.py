4d0aed2f5d1972177742efa2a87327143dc90cf6
enzun123 <enzun123@gmail.com>
2026-03-30 09:42:02 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -62,4 +62,7 @@ sns.regplot(x='total_bill', y='tip', data=tips, color='blue', scatter_kws={'s': 
 fig.suptitle('Scatter Plots with Regression Lines', fontsize=16)
 ax1.set_title('fit_reg = True')
 ax2.set_title('fit_reg = False')
-plt.savefig('')
+plt.savefig('./week04/Seaborn_Figure03.jpg')
+
+# ** 히스토그램과 커널 밀도 추정 그래프**
+# 히스토그램과 
