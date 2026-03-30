409d5b6ac731c92b877294ccd357b0021408bca8
enzun123 <enzun123@gmail.com>
2026-03-30 10:30:11 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -51,4 +51,5 @@ from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(strategy="median")
 
 # 수치형 특성만 추출
-housing_mum = hou
+housing_mum = housing.select_dtypes(include=[np.number])
+housing
