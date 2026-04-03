e9b89a7d53728905a0b7235c8fc8329406b3f09a
enzun123 <enzun123@gmail.com>
2026-04-03 15:01:20 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -11,4 +11,9 @@ from sklearn.model_selection import train_test_split
 
 housing["income_cat"] = pd.cut(housing["median_income"],
                                bin6s=[0., 1.5, 3.0, 4.5, 6., np.inf],
-                               labels=[1, 2, 3, 4, ])
+                               labels=[1, 2, 3, 4, 5])
+
+
+start_train_set, start_test_set = train_test_split(
+    
+)
