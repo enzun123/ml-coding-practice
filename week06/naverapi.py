ff1a4db68abca56aa6dc086fabc6ced6efbfcffd
enzun123 <enzun123@gmail.com>
2026-04-09 10:16:19 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -27,5 +27,6 @@ def main():
 
     print('전체 검색 : %d 건' %total)
     
-    with open('%s_naver%s.json' % (srcText, node), 'w', encoding = 'utf-8' )
+    with open('%s_naver%s.json' % (srcText, node), 'w', encoding = 'utf8') as outfile:
+        jsonFile = json.dum
 
