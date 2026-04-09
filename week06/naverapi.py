74ce3e36a0443ed2f1339f7a079b3db5e8224f72
enzun123 <enzun123@gmail.com>
2026-04-09 10:41:27 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -45,3 +45,7 @@ def getNaverSearch(node, srcText, page_start, display):
     url = base + node + parameters
     responseDecode = getRequestUrl(url)         #   [CODE 1]
     
+    if (responseDecode == None):
+        return None
+    else:
+        r
