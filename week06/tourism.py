0819ab1b4a1ca271a8de1e23aadbfaa94fe4a9c5
enzun123 <enzun123@gmail.com>
2026-04-11 20:42:59 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -19,6 +19,6 @@ def main():
     ed_cd = "E"
     
     jsonResult, result, natName, dateEnd = getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear)  #[CODE 3]
-    
+     
     #파일저장 : csv 파일
-    columns = [""]
+    columns = ["입극자국가", "국가코드", "입국연월", "입국자 ㅜㅅ"]
