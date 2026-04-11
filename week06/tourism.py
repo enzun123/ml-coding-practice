7d85d9d477a9b14d369e0d50163a38a4860ab5da
enzun123 <enzun123@gmail.com>
2026-04-11 20:37:59 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -4,4 +4,9 @@ import datetime
 import json
 import pandas as pd
 
-ServiceKey = ""
+ServiceKey = "1e274d34339735d4a3485ee5aed58c21abf015f79b9b6e5cfe05b84001007baa"
+
+"""### [CODE 0]"""
+
+def main():
+    jsonResult 
