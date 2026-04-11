367920a42a75423e110a901c7bc7ca642dfd05bb
enzun123 <enzun123@gmail.com>
2026-04-11 20:49:29 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -35,4 +35,4 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
         for month in range(1, 13):
             yyyymm = "{0}{1:0>2}".format(str(year), str(month))
             jsonData = getTourismStatsService(yyyymm, nat_cd, ed_cd)        #[CODE 2]
-            if (jsonData)
+            if (jsonData['response']['header']['re'])
