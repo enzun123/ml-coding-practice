c9bd826d47633cc0bfa91b5bb5607778c8c743d7
enzun123 <enzun123@gmail.com>
2026-04-28 01:50:39 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -88,4 +88,7 @@ from sklearn.metrics import roc_auc_score
 
 y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
                              method="decision_function")
-roc
+roc_auc_score(y_train_5, y_scores)
+
+# 다음을 실행하는데 몇 분이 걸릴수 있음
+from sk
