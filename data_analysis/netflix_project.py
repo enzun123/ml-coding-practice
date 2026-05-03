192aeb454d2ab25b50bb2f56680603dd651d5e9e
enzun123 <enzun123@gmail.com>
2026-05-03 19:04:14 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -184,3 +184,5 @@ plt.figure(figsize=(15, 5))
 cmap = plt.matplotlib.color.LinearSegmentedColormap.from_list('',['#221f1f','#b20710','#f5f5f1'])
 
 sns.heatmap(netflix_age_country_unstack, cmap = cmap, linewidths=2.5, annot=True, fmt='.0%')
+
+plt.suptitle('Target ages proportion of total content b')
