d121c45abd259729944e7aeb2c66211ff8644a33
enzun123 <enzun123@gmail.com>
2026-05-03 19:14:44 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -192,6 +192,7 @@ plt.title('Here we see interesting differences between countries. Most shows in 
 plt.show
 
 # 넷플릭스 데이터의 description 열 이용한 워드 클라우드 생성
+
 # WordCloud : 워드 클라우드 생성에 필요한 모듈
 # Image : 워드 클라우드를 원하는 형태로 그리기 위해 그림을 불러오는 패키지
 from wordcloud import WordCloud
@@ -208,3 +209,4 @@ mask = np.array(Image.open('netflix_logo.jpg'))
 
 # 워드 클라우드 색상맵 만들기
 cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#b20710'])
+
