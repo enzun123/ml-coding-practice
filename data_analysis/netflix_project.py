953283227d7801dde79e1ca9a7f3d28576c7405d
enzun123 <enzun123@gmail.com>
2026-05-03 19:17:44 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -215,4 +215,5 @@ cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#
 wordcloud = WordCloud(background_color = 'white', width = 1400, height = 1400,
                       max_words = 170, mask = mask, colormap=cmap).generate(text)
 
-p
+plt.suptitle('Keywords in the description of Movies and TV shows',
+             fontweighr='bold')
