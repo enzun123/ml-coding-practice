a8a7922c4fb5d21fac5cf2d3ccf0c757d215f23e
enzun123 <enzun123@gmail.com>
2026-05-10 11:46:46 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -38,4 +38,5 @@ print(lin_reg.predict(X_new))
 from sklearn.linear_model import SGDRegressor
 
 sgd_reg = SGDRegressor(penalty="l2", alpha=0.1 / m, tol=None,
-                       ,max_iter=1000, e)
+                       max_iter=1000, eta0=0.01, random_state=42)
+sgd_reg.fit(X, y)
