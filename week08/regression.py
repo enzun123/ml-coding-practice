df9de71882ff959c14f876dd5cd18d24cb58324b
enzun123 <enzun123@gmail.com>
2026-05-10 12:13:46 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -142,5 +142,8 @@ list(iris)
 print(iris.data.head(3))
 print(iris.target.head(3))  # 샘플이 섞여 있지 않습니다.
 
-print(iris.target.head(3))
+print(iris.target.names)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.mo
 
