43e09af133d0552e5090223337f6daf6f0236514
enzun123 <enzun123@gmail.com>
2026-05-10 12:54:17 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -53,7 +53,7 @@ from sklearn.svm import LinearSVR
 # 이 세 줄은 간단한 선형 데이터셋을 생성합니다.
 np.random.seed(42)
 X = 2 * np.random.rand(50, 1)
-y = 4 + 3 * X[:, 0] + np.random.random(50)
+y = 4 + 3 * X[:, 0] + np.random.randn(50)
 
 svm_reg = make_pipeline(StandardScaler(),
                         LinearSVR(epsilon=0.5, dual=True, random_state=42))
